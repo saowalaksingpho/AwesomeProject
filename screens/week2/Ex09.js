90b0d63360482155c44bb3f120c75cd2f494a67b
saowalaksingpho <saowalak.sing@vru.ac.th>
2023-11-30 13:47:33 +0700

    Initial commit

---
+++ screens/week2/Ex09.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex09() {
+  return (
+
+<View style={{ flex : 1 , flexDirection : 'row',justifyContent : 'space-between' , alignItems : 'center'}}>    
+      {/* <View style={{ backgroundColor : '#50E3C2' ,height:100, width : 100  }}></View>    
+      <View style={{ backgroundColor : '#4A90E2' , height:100, width : 100  }}></View>  
+      <View style={{ backgroundColor : '#9013FE' ,height:100, width : 100  }}></View>     */}
+    </View>
+  
+  );
+
+}
