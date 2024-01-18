d6cf041796ae0d78ea01083fe8c59802b099a34a
saowalaksingpho <saowalak.sing@vru.ac.th>
2024-01-18 16:42:11 +0700

    week9

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -5,6 +5,10 @@ import PokemonTab from "./PokemonTab";
 import Travel from "../screens/week3/Travel";
 import Resort from "../screens/week4/Resort";
 import Health from "../screens/week5/Health";
+import Book from "../screens/week9/Book";
+import BookDetail from "../screens/week9/BookDetail";
+import BookForm from "../screens/week9/BookForm";
+import TodoList from "../screens/week9/TodoList";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +20,10 @@ export default function RootStack() {
             <Stack.Screen name="Travel" component={Travel} options={{ title: "Travel" }} />
             <Stack.Screen name="Resort" component={Resort} options={{ title: "Resort" }} />
             <Stack.Screen name="Health" component={Health} options={{ title: "Health" }} />
+            <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+            <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+            <Stack.Screen name="BookForm" component={BookForm} />
+            <Stack.Screen name="TodoList" component={TodoList} />
         </Stack.Navigator>
     );
 }
